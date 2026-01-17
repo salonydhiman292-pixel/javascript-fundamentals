f3acb591068808b5d8670616f53ffdceb3895d95
Salony DHiman <salonydhiman292@gmail.com>
2026-01-17 18:04:24 +0530

    Add files via upload

---
+++ revision2.js
@@ -0,0 +1,58 @@
+let students=["aman","shradha","rajat","salony","kartik","harsh"];
+console.log(students);
+
+// practice question
+let arr=["january","july","march","august"];
+console.log(arr.shift());
+console.log(arr.shift());
+console.log(arr.unshift("june"));
+console.log(arr.unshift("july"));
+console.log(arr);
+// question
+let month=["january","july","march","august"];
+console.log(month.splice(0,2,"july","june"));
+console.log(month);
+// question
+let lang=["c","c++","html","javascript","python","java","c#","sql"];
+console.log(lang.reverse().indexOf("javascript"));
+// practice
+let game=[['X',null,'O'],[null,'X',null],['O',null,'X']];
+console.log(game);
+
+
+let s=["salony" ,"aakarti" ,"sweekarti" ,"bhoovika"]
+console.log(s);
+let car=["bmw","audi","toyota","shift"]
+console.log(car);
+let m=["january","july","march","august"]
+console.log(m);
+
+let student1={ name: "arnav", class: "play", rollno: 1 };
+console.log(student1);
+let student2={name: "bhoovika", class: "second", rollno: 2};
+console.log(student2);
+let student3={name: "aakarti", class: "11th", rollno:3};
+console.log(student3);
+let student4={name: "sweekarti", class: "6th", rollno: 4};
+console.log(student4);
+
+let stu=["student1","student2","student3","student4"];
+
+console.log(stu[0]); 
+console.log(stu[1]);
+console.log(stu[2]);3.
+console.log(stu[3]);
+
+console.log(student1.name); 
+console.log(student1.name);
+console.log(student2.class);
+console.log(student3.rollno);
+console.log(student4.name);
+
+let heroes=[["ironamn","spiderman","thor"],["wonder women","feash"]];
+console.log(heroes);  
+
+let fruits=["mango","apple",'banana',"litchi","orange"];
+for(fruit of fruits){
+    console.log(fruit);
+}
