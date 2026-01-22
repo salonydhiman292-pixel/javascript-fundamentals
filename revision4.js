859cdb55056aaa7a06e9fa5a74164b3ad1fb6a4e
Salony DHiman <salonydhiman292@gmail.com>
2026-01-22 20:58:52 +0530

    Add files via upload

---
+++ revision4.js
@@ -0,0 +1,29 @@
+// js objects literals 
+let delhi={
+    Latitude: "28.7041 N",
+    Longitude: "77.1025 E" 
+};
+
+const student6={
+    name: "salony",
+    age:21,
+    marks: 80.0,
+    city: "Delhi"
+};
+console.log(student6.city="Mumbai");
+console.log(delete student6.marks);
+console.log(student6);
+
+const item={
+    price: 100.90,
+    discount: 50,
+    colors: ["red","pink"]
+};
+
+const post={
+    username: "@salonydhiman",
+    content: "this is my #first post",
+    likes: 50,
+    reports: 5,
+    tags: ["@webdev","@fire"]
+};
